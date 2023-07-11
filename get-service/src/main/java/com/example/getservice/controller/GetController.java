bcf5fb5d9a0f061ec3ea222366fb29d1a1c7b776
lukas1299 <lukaszmatusik14@gmail.com>
2023-07-11 08:36:47 +0200

    Minor changes

--- get-service/src/main/java/com/example/getservice/controller/GetController.java
+++ get-service/src/main/java/com/example/getservice/controller/GetController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/get")
+@RequestMapping("/fetched-persons")
 public class GetController {
 
     private final PersonServiceUtil personServiceUtil;
